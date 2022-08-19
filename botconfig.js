d4c0d6bc96c365b8bda85243689302b76bc56407
ALFiX01 <boyarov.danil71@mail.ru>
2022-08-19 13:17:52 +0300

    2.1

--- botconfig.js
+++ botconfig.js
@@ -4,7 +4,7 @@ module.exports = {
   DefaultPrefix: process.env.Prefix || ">", // Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/sbySMS7m3v", // Support Server Link
-  Token: process.env.Token || "", // Discord Bot Token
+  Token: process.env.Token || "MTAxMDEyNjE3MjIyODY5ODE5Mg.GOMTLN.s88IfWik2BSdTUeq1OzgwaEe-R5aiWibJkvIYI", // Discord Bot Token
   ClientID: process.env.Discord_ClientID || "", // Discord Client ID
   ClientSecret: process.env.Discord_ClientSecret || "", // Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], // Discord OAuth2 Scopes
