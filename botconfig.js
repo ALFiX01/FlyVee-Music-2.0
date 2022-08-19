4de2ae49e992ae33c7dfdfca4fac21d8cd8944a0
ALFiX01 <boyarov.danil71@mail.ru>
2022-08-19 13:25:51 +0300

    2.3.1

--- botconfig.js
+++ botconfig.js
@@ -22,7 +22,7 @@ module.exports = {
 
   Presence: {
     status: "online", // You can show online, idle, and dnd
-    name: "Music | 2.3", // The message shown
+    name: "Music | 2.3.1", // The message shown
     type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING
   },
 
@@ -30,9 +30,9 @@ module.exports = {
   // Lavalink server; public lavalink -> https://lavalink-list.darrennathanael.com/; create one yourself -> https://darrennathanael.com/post/how-to-lavalink
   Lavalink: {
     id: "Main", //- Used for indentifier. You can set this to whatever you want.
-    host: "lavalink.oops.wtf", //- The host name or IP of the lavalink server.
-    port: 2000, // The port that lavalink is listening to. This must be a number!
-    pass: "www.freehosts.ga", //- The password of the lavalink server.
+    host: "lavalink.cloudblue.ml", //- The host name or IP of the lavalink server.
+    port: 1555, // The port that lavalink is listening to. This must be a number!
+    pass: "lava", //- The password of the lavalink server.
     secure: false, // Set this to true if the lavalink uses SSL. if not set it to false.
     retryAmount: 200, //- The amount of times to retry connecting to the node if connection got dropped.
     retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
